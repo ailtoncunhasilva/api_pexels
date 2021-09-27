c35e337467e5569b38014fac790777458108e8ed
ailtoncunhasilva <ailtoncunhasilva@hotmail.com>
2021-09-27 20:09:11 -0300

    one more adjustment

--- lib/app/screens/showdialog_details.dart
+++ lib/app/screens/showdialog_details.dart
@@ -22,7 +22,7 @@ class ShowDialogDetails extends StatelessWidget {
                   color: Colors.white,
                 ),
                 image: DecorationImage(
-                  image: NetworkImage(photo?.src?.original ?? ''),
+                  image: NetworkImage(photo?.src?.small ?? ''),
                   fit: BoxFit.cover,
                 ),
               ),
